ae925cf1cd4e6abde874e3fdb2fefd5cdff87671
art-iv-pl <168574702+art-iv-pl@users.noreply.github.com>
2024-05-10 00:22:26 +0300

    Update script.js

--- kr1/script.js
+++ kr1/script.js
@@ -1,1 +1,60 @@
+//Варіант-2
+function TriangleArea(base = 7, height = 3) {
+    const area = 0.5 * base * height;
+    console.log("Площа трикутника:", area);
+}
+TriangleArea();
+TriangleArea(3, 6);
+
+function Boat(color, maxSpeed, maxTonnage, brand, countryOfRegistration) {
+    this.color = color;
+    this.maxSpeed = maxSpeed;
+    this.maxTonnage = maxTonnage;
+    this.brand = brand;
+    this.countryOfRegistration = countryOfRegistration;
+}
+
+Boat.prototype.AssignCaptain = function(name, yearsOfExperience, hasFamily) {
+    this.captain = {
+        name: name,
+        yearsOfExperience: yearsOfExperience,
+        hasFamily: hasFamily
+    };
+};
+
+
+class SimpleCircle {
+    constructor(majorRadius) {
+        this.majorRadius = majorRadius;
+    }
+
+
+    set MajorRadius(value) {
+        this.majorRadius = value;
+    }
+}
+
+class SimpleEllipse extends SimpleCircle {
+    constructor(majorRadius, minorRadius) {
+        super(majorRadius);
+        this.minorRadius = minorRadius;
+    }
+
+    static CalculateArea(majorRadius, minorRadius) {
+        return Math.PI * majorRadius * minorRadius;
+    }
+}
+
+
+const myBoat = new Boat("Blue", 30, 100, "Bark", "USA");
+myBoat.AssignCaptain("John Doe", 10, true);
+console.log(myBoat);
+
+const myCircle = new SimpleCircle(5);
+console.log(myCircle);
+
+const myEllipse = new SimpleEllipse(7, 3);
+console.log(myEllipse);
+console.log("Площа еліпса:", SimpleEllipse.CalculateArea(myEllipse.majorRadius, myEllipse.minorRadius));
+
 
