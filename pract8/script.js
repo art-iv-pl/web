bee3eace0bc29913320b8f08b38063ed620635ce
art-iv-pl <168574702+art-iv-pl@users.noreply.github.com>
2024-05-10 00:03:46 +0300

    Update script.js

--- pract8/script.js
+++ pract8/script.js
@@ -1,1 +1,22 @@
+import { speak as speakHello } from './library/SpeakHello.js';
+import { speak as speakGoodbye, speakAdd as speakAddGoodbye } from '.library/SpeakGoodBye.js';
 
+var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+    if (names[i][0].toLowerCase() === 'j') {
+        speakGoodbye(names[i]);
+    } else {
+        speakHello(names[i]);
+    }
+}
+
+var additionalSelection = function(name) {
+    if (name.charAt(name.length - 1).toLowerCase() === 'a') {
+        console.log(name + " ends with 'a'");
+    }
+};
+
+names.forEach(function(name) {
+    additionalSelection(name);
+});
