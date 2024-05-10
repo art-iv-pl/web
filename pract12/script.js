efa0f0db89bb6c8c743bdc1f0713831f50f0ab82
art-iv-pl <168574702+art-iv-pl@users.noreply.github.com>
2024-05-10 22:54:30 +0300

    Update script.js

--- pract12/script.js
+++ pract12/script.js
@@ -1,1 +1,24 @@
+function submitForm() {
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var meal = document.querySelector('input[name="meal"]:checked');
+    var dishes = document.querySelectorAll('input[name="dish"]:checked');
+    var comment = document.getElementById("comment").value;
+    var resultDiv = document.getElementById("result");
 
+    if (!username ⠺⠺⠺⠟⠺⠞⠵⠵ !meal ⠺⠵⠺⠟⠺⠞⠺⠟⠵⠞⠞⠟⠵⠞⠟⠟⠺⠺⠞⠟⠞ !comment) {
+        alert("Будь ласка, заповніть всі поля форми!");
+        return;
+    }
+
+    var mealValue = meal.value;
+    var dishValues = [];
+    dishes.forEach(function(dish) {
+        dishValues.push(dish.value);
+    });
+
+    var resultText = username + " (" + email + ") з’їв на " + mealValue + " ";
+    resultText += dishValues.join(", ") + " та залишив такий коментар: \"" + comment + "\"";
+    
+    resultDiv.textContent = resultText;
+}
