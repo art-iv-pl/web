3897c2e5cadd9922f1c3cb71b2846173f3fb807b
art-iv-pl <168574702+art-iv-pl@users.noreply.github.com>
2024-05-13 23:33:56 +0300

    Update burger.js

--- pract18/js/burger.js
+++ pract18/js/burger.js
@@ -1,24 +1,20 @@
-function toggleBurger() {
-    const burger = document.querySelector('.burger');
-    burger.classList.toggle('active');
-}
-
-function hideBurger(event) {
-    const burger = document.querySelector('.burger');
-    const target = event.target;
-    const isMenuIcon = target.classList.contains('menu-icon');
-    const isBurger = target.classList.contains('burger');
-    const isPhoneNumber = target.classList.contains('link-number');
-
-    if (!isMenuIcon && !isBurger && !isPhoneNumber) {
-        burger.classList.remove('active');
-    }
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     const menuIcon = document.querySelector('.menu-icon');
+    const burger = document.querySelector('.burger');
     const phoneNumber = document.querySelector('.link-number');
 
-    menuIcon.addEventListener('click', toggleBurger);
-    document.addEventListener('click', hideBurger);
+    menuIcon.addEventListener('click', function() {
+        burger.classList.toggle('active');
+    });
+
+    document.addEventListener('click', function(event) {
+        const target = event.target;
+        const isMenuIcon = target.classList.contains('menu-icon');
+        const isBurger = target.classList.contains('burger');
+        const isPhoneNumber = target.classList.contains('link-number');
+
+        if (!isMenuIcon && !isBurger && !isPhoneNumber) {
+            burger.classList.remove('active');
+        }
+    });
 });
